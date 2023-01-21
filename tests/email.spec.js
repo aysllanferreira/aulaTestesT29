24e73f3c5008815a1f0a1b5d64f3e296bdd8ac62
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-21 16:49:56 -0300

    req: 01 - feito.

--- tests/email.spec.js
+++ tests/email.spec.js
@@ -1,5 +1,11 @@
 const createEmail = require('../functions/emailCreator');
 
 describe('Teste da função createEmail', () => {
-  
+  it('Testa se a funcao createEmail foi definida', () => {
+    expect(createEmail).toBeDefined();
+  })
+
+  test('Testar se o retorno da funcao esta correto', () => {
+    expect(createEmail('Aysllan', 'Ferreira')).toBe('aysllan_ferreira@aulao.com')
+  })
 });
