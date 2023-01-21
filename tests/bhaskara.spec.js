5d5739fb65d3ac19ff5330c4535c9e564c67dbe8
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-21 16:23:10 -0300

    Subindo resolucoes

--- tests/bhaskara.spec.js
+++ tests/bhaskara.spec.js
@@ -1,5 +1,15 @@
 const bhaskara = require('../functions/bhaskara');
 
 describe('Teste da função bhaskara', () => {
-  
+  it('Teste se a função bhaskara é definida', () => {
+    expect(bhaskara).toBeDefined();
+  });
+
+  it('Teste se a função bhaskara retorna o valor de x1', () => {
+    expect(bhaskara(1, 3, 2)).toEqual([-1, -2]);
+  });
+
+  it('Teste se a função bhaskara retorna o valor de x2', () => {
+    expect(bhaskara(3, 1, 2)).toEqual('Delta é negativo');
+  });
 });
--- tests/equals.spec.js
+++ tests/equals.spec.js
@@ -1,6 +1,16 @@
 const equals = require('../functions/equals');
 
 describe('Teste da função equals', () => {
- 
+  it('Teste se a função equals é definida', () => {
+    expect(equals).toBeDefined();
+  });
+
+  it('Teste se a função equals retorna true quando os parâmetros são iguais', () => {
+    expect(equals(1, 1)).toBe(true);
+  });
+
+  it('Teste se a função equals retorna false quando os parâmetros são diferentes', () => {
+    expect(equals(1, 2)).toBe(false);
+  });
 });
 
