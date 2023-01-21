5779ce7cfaf911569a587cd64745240b43251619
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-21 16:54:41 -0300

    req: 02 - feito

--- tests/equals.spec.js
+++ tests/equals.spec.js
@@ -1,6 +1,16 @@
 const equals = require('../functions/equals');
 
 describe('Teste da função equals', () => {
- 
+  it('Funcao equals esta definida', () => {
+    expect(equals).toBeDefined();
+  })
+
+  it('Verifica se passados parametros identicos, o retorno da funcao retornara True', () => {
+    expect(equals(3, 3)).toBe(true);
+  })
+
+  it('Verifica se passados parametros nao identicos, o retorno da funcao retornara False', () => {
+    expect(equals(3, 4)).toBe(false);
+  })
 });
 
