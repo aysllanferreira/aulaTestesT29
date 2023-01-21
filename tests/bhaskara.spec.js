bbd6f705f8e97b8aa334e9a6892b32afd1c5973a
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-21 17:27:22 -0300

    req: 05 e 06 -- feito

--- tests/bhaskara.spec.js
+++ tests/bhaskara.spec.js
@@ -1,5 +1,21 @@
 const bhaskara = require('../functions/bhaskara');
 
 describe('Teste da função bhaskara', () => {
-  
+  it('Teste da funcao de Bhaskara', () => {
+    expect(bhaskara).toBeDefined();
+  })
+
+  it('Testa se o delta eh negativo', () => {
+    expect(bhaskara(2, 1, 2)).toBe('Delta é negativo')
+  })
+
+  it('Teste se a funcao retorna o valor esperado pelo return', () => {
+    expect(bhaskara(1, 3, 2)).toStrictEqual([-1, -2])
+  })
+
+  it('Teste se a funcao retorna o valor esperado pelo return', () => {
+    expect(bhaskara(1, 3, 2)).toEqual([-1, -2])
+  })
 });
+
+// Tratando o retorno com array: use toEqual ou toStrictEqual
